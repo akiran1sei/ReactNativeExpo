76e27e8d7ba65878a3cddd9b7b8acb06c7350fe9
akiran1sei <nakamori.1224@gmail.com>
2025-03-15 19:49:39 +0900

    (tabs)フォルダを作成まで（２周目）

--- Tutorial/app/index.tsx
+++ Tutorial/app/index.tsx
@@ -1,15 +1,26 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World.</Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#7D4A31",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 18,
+  },
+  button: { fontSize: 20, textDecorationLine: "underline", color: "#fff" },
+});
