8ce43fc335d8d1fdc4ad95c50ae187b04c6e606b
akiran1sei <nakamori.1224@gmail.com>
2025-03-25 14:55:35 +0900

    レーダーチャートのサンプル作成に成功

--- Tutorial/components/RadarChart/RadarChart.tsx
+++ Tutorial/components/RadarChart/RadarChart.tsx
@@ -1,12 +1,43 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+import {
+  VictoryPolarAxis,
+  VictoryChart,
+  VictoryTheme,
+  VictoryLine,
+} from "victory-native";
+const data = [
+  { x: "酸味", y: 1 },
+  { x: "苦味", y: 0.5 },
+  { x: "甘味", y: 1 },
+  { x: "コク", y: 1.5 },
+  { x: "香り", y: 2.5 },
+  { x: "後味", y: 3.5 },
+  { x: "酸味", y: 1 }, // 閉じるための最初のデータを再度追加
+];
 
 const RadarChart = () => {
   return (
-    <View>
-      <Text>Radar Chart</Text>
+    <View style={styles.container}>
+      <VictoryChart polar domain={{ y: [0, 5] }} theme={VictoryTheme.clean}>
+        <VictoryPolarAxis
+          dependentAxis
+          style={{ axis: { stroke: "none" } }}
+          tickFormat={data}
+        />
+        <VictoryPolarAxis />
+        <VictoryLine data={data} />
+      </VictoryChart>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
 
 export default RadarChart;
